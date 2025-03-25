236d91ccaf4f2d3811dc4569ee60fc379324ea50
Patrick Stigler <patrick@stigler.de>
2025-03-25 15:08:25 +0100

    added google analytics

---
+++ public/assets/js/google-analytics.js
@@ -0,0 +1,8 @@
+// Google Analytics 4 Tracking Code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.googletagmanager.com/gtag/js?id=G-5V5NY3VXFY','ga');
+
+ga('create', 'G-5V5NY3VXFY', 'auto');
+ga('send', 'pageview'); 
